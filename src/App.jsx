f617b2db530376fe2e41e0fb913c30d597f7c0a9
Emmanuel Diatta <80013080+Lowkey-Emmanuel@users.noreply.github.com>
2022-09-01 15:57:04 +0000

    Adding Header/Main/Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Header from './Header.jsx'
+import Main from './Main.jsx'
+import Footer from './Footer.jsx'
 
 export default function App() {
   return (
-    <main>
-      HELLO WORLD ⚡
-    </main>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
